0e1536f7a2c2124d33042651e2c7d0a71789e998
Bulgun <bulgunya@mail.ru>
2024-05-10 23:38:00 +0300

    add client, pom.xml, main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите своё имя: ");
+
+        String name = scanner.nextLine();
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            System.out.println("Connect To:" + address + ":" + 8800);
+            Socket socket = new Socket(address, 8080);
+            Client client = new Client(socket, name);
+            InetAddress inetAddress = socket.getInetAddress();
+            System.out.println("InetAddress: " + inetAddress);
+            String remoteIP = inetAddress.getHostAddress();
+            System.out.println("Remote IP: " + remoteIP);
+            System.out.println("LocalPort: " + socket.getLocalPort());
+
+            client.ListenForMessages();
+            client.sendMessage();
+
+        }
+        catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        catch (IIOException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+
+        }
+    }
+}
